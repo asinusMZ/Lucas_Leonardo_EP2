1a3824a3a54ec7115a2a855cfd7103414cc21156
Lucas Queiroz <lucaslq1807@gmail>
2026-04-16 10:20:14 -0300

    Guardar dado 1 - Lucas Queiroz

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,6 @@
 import random
 
+
 def rolar_dados(quantidade_dados):
     lista_dados = []
     for dado in range(quantidade_dados):
@@ -7,4 +8,13 @@ def rolar_dados(quantidade_dados):
         lista_dados.append(valor_dado)
     return lista_dados
 
-        
+def guardar_dado(lista_dados, dado_escolhido):
+    dados_armazenados = []
+    lista_dados_restantes = []
+    for dado in lista_dados:
+        if dado == dado_escolhido:
+            dados_armazenados.append(dado)
+        elif dado != dado_escolhido:
+            lista_dados_restantes.append(dado)
+    return dados_armazenados, lista_dados_restantes
+    
