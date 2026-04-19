eb013155e1755149cec33c58c0e69e3c276bd6c7
Leonardo Marsolla <marsolla.leonardo@gmail.com>
2026-04-18 23:56:22 -0300

    Guardar dados 6 - Leonardo Marsolla

--- funcoes.py
+++ funcoes.py
@@ -8,18 +8,9 @@ def rolar_dados(quantidade_dados):
         lista_dados.append(valor_dado)
     return lista_dados
 
-def guardar_dado(lista_dados, dados_armazenados, dado_escolhido):
-    dados_restantes = []
-    lista_final_dados = []
-    for dado in range(lista_dados):
-        if lista_dados[dado] == dado_escolhido:
-            dados_armazenados.append(dado)
-        elif lista_dados[dado] != dado_escolhido:
-            dados_restantes.append(dado)
+def guardar_dado(rolados, guardados, indice):
+    guardados.append(rolados[indice])
+    rolados.pop(indice)
 
-
-    lista_final_dados[0] = dados_restantes
-    lista_final_dados[1] = dados_armazenados
-
-    return lista_final_dados
+    return = [rolados, guardados]
     
